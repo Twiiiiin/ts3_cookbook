885d7044aa11d2b0373ed3c18e153f0683a8bad3
Twiiiiin <twin.flavio@gmail.com>
2016-11-07 15:51:23 +0100

    Test patches

--- recipes/default.rb
+++ recipes/default.rb
@@ -34,48 +34,11 @@ end
 
 log "Configuring restart script"
 
-file '/etc/init.d/teamspeak' do 
-	action :create 
-	content '#!/bin/sh
-### BEGIN INIT INFO
-# Provides: teamspeak
-# Required-Start: $local_fs $network
-# Required-Stop: $local_fs $network
-# Default-Start: 2 3 4 5
-# Default-Stop: 0 1 6
-# Description: Teamspeak 3 Server
-### END INIT INFO
-
-USER="teamspeak"
-DIR="/home/teamspeak/teamspeak3-server_linux-amd64"
-###### Teamspeak 3 server start/stop script ######
-case "$1" in
-start)
-su $USER -c "$DIR/ts3server_startscript.sh start"
-;;
-stop)
-su $USER -c "$DIR/ts3server_startscript.sh stop"
-;;
-restart)
-su $USER -c "$DIR/ts3server_startscript.sh restart"
-;;
-status)
-su $USER -c "$DIR/ts3server_startscript.sh status"
-;;
-*)
-echo "Usage: " >&2
-exit 1
-;;
-esac
-exit 0'
-	mode '0700'
-	owner 'root'
-end
-
 service 'teamspeak' do
 	supports :reload => true
 	init_command '/etc/init.d/teamspeak'
-	action [:reload,:enable,:start]
+	subscribes :reload, 'template[teamspeak]', :immediately
+	action [:enable,:start]
 end
 
 
